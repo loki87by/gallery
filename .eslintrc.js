116c66246758906d508591cfddbfe58f9144d526
Alexey Akulich <loki1987by@yandex.ru>
2021-06-04 14:34:48 +0300

    init: add & setup environment files

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+      "browser": true,
+      "es2021": true,
+      "node": true
+    },
+    "extends": [
+      "eslint:recommended",
+      "plugin:prettier/recommended",
+      "prettier",
+    ],
+    "parserOptions": {
+      "ecmaVersion": 2020,
+      "sourceType": "module"
+    },
+    "plugins": [
+      "prettier",
+      "import"
+    ],
+    "ignorePatterns": ["/node_modules/*.js"],
+    "rules": {
+    }
+  };
